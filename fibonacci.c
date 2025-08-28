885d6adf3ab28f34568749799872a9bb001046ec
AynSgh <ays103569@gmail.com>
2025-08-28 17:54:26 +0530

    fibonacci series program

---
+++ fibonacci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+int main(int argc,char **argv){
+	uint16_t x=0;
+	uint16_t y=1;
+	uint16_t z=0;
+	while(x<=255){
+		printf("%"PRIu16"\n",x);
+		z=x+y;
+		x=y;
+		y=z;
+	}
+	return;
+}
+
